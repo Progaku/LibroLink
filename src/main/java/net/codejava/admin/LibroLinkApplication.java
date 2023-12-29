8fc250d6c2c3ede613c893347f3817d337778b22
rsryo <rs.ryoichi.09@gmail.com>
2023-12-29 19:36:47 +0900

    first commit

---
+++ src/main/java/net/codejava/admin/LibroLinkApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/codejava/admin/LibroLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
